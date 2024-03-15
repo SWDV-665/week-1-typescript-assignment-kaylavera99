9cab67647c395b54e8fcb86fff424091cfb23ebd
kaylavera99 <kaylavd99@gmail.com>
2024-03-15 16:26:28 -0400

    Changes #1
    Added styling and corrected title. List is now centered, with bubble list item bullets and the font style is changed to sans-serif

--- groceries.js
+++ groceries.js
@@ -1,21 +1,38 @@
 var Grocery = /** @class */ (function () {
-    function Grocery(item, quantity, dept) {
+    function Grocery(item, quantity, unit, dept) {
         this.item = item;
         this.quantity = quantity;
+        this.unit = unit;
         this.dept = dept;
     }
     return Grocery;
 }());
 var cart = [
-    new Grocery("Banana", 10, "Produce"),
-    new Grocery("Carrot", 5, "Produce"),
-    new Grocery("Milk", 1, "Dairy"),
-    new Grocery("Bread", 2, "Bakery"),
-    new Grocery("Grilled Chicken", 4, "Deli"),
-    new Grocery("Gouda", 1, "Deli")
+    new Grocery("Banana", 1.5, "", "Produce"),
+    new Grocery("Carrot", 5, "", "Produce"),
+    new Grocery("Milk", 1, "", "Dairy"),
+    new Grocery("Bread", 2, "", "Bakery"),
+    new Grocery("Grilled Chicken", 4, "", "Deli"),
+    new Grocery("Gouda", 1, "", "Deli"),
+    new Grocery("Ranch Dressing", 1, "", "Condiments"),
+    new Grocery("Steak", 4, "", "Deli"),
+    new Grocery("Gouda", 1, "", "Deli")
 ];
+for (var j = 0; j < cart.length; j++) {
+    if (cart[j].dept === "Deli" || cart[j].dept === "Produce") {
+        if (cart[j].quantity > 1) {
+            cart[j].unit = "pounds";
+        }
+        else {
+            cart[j].unit = "pound";
+        }
+    }
+    else {
+        continue;
+    }
+}
 for (var i = 0; i < cart.length; i++) {
-    if (cart[i].quantity > 1) {
+    if (cart[i].quantity > 1 && (cart[i].dept === "Produce")) {
         var mult = "s";
         cart[i].item = cart[i].item + mult;
     }
@@ -28,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (groceryList) {
         cart.forEach(function (grocery) {
             var listItem = document.createElement("li");
-            listItem.textContent = "".concat(grocery.quantity, " ").concat(grocery.item, " ").concat(grocery.dept);
+            listItem.textContent = "".concat(grocery.item, ",       ").concat(grocery.quantity, "         ").concat(grocery.unit, ",       ").concat(grocery.dept);
             groceryList.appendChild(listItem);
         });
     }
--- groceries.ts
+++ groceries.ts
@@ -1,28 +1,47 @@
 class Grocery {
 	item: string;
 	quantity: number;
+	unit: string;
 	dept: string;
+	
 
-	constructor(item:string, quantity:number, dept:string) {
+	constructor(item:string, quantity:number,unit:string, dept:string) {
 		this.item = item;
 		this.quantity = quantity;
+		this.unit = unit;
 		this.dept = dept;
 	}
 }
 
 
 let cart: Grocery[] = [
-	new Grocery("Banana", 10, "Produce"),
-	new Grocery("Carrot", 5, "Produce"),
-	new Grocery("Milk", 1, "Dairy"),
-	new Grocery("Bread", 2, "Bakery"),
-	new Grocery("Grilled Chicken", 4, "Deli"),
-	new Grocery("Gouda", 1, "Deli")
+	new Grocery("Banana", 1.5, "", "Produce"),
+	new Grocery("Carrot", 5,"", "Produce"),
+	new Grocery("Milk", 1, "", "Dairy"),
+	new Grocery("Bread", 2, "", "Bakery"),
+	new Grocery("Grilled Chicken", 4, "", "Deli"),
+	new Grocery("Gouda", 1, "","Deli"),
+	new Grocery("Ranch Dressing", 1,"", "Condiments"),
+	new Grocery("Steak", 4, "","Deli"),
+	new Grocery("Gouda", 1,"", "Deli")
 	];
 
+for (let j = 0; j < cart.length; j++){
+	if (cart[j].dept === "Deli" || cart[j].dept === "Produce") {
+		if (cart[j].quantity > 1) {
+			cart[j].unit = "pounds"
+		}
+		else {
+			cart[j].unit = "pound"
+		}
+		
+	} else {
+		continue;
+	}
+}
 
 for (let i = 0; i < cart.length; i ++){
-	if (cart[i].quantity > 1){
+	if (cart[i].quantity > 1 && (cart[i].dept === "Produce")){
 		let mult = "s";
 		cart[i].item = cart[i].item + mult;
 	}
@@ -30,14 +49,17 @@ for (let i = 0; i < cart.length; i ++){
 		continue;
 
 	}
+
 }
+
+
 
 document.addEventListener("DOMContentLoaded", () => { 
     const groceryList = document.getElementById("grocery_list");
     if (groceryList) {
         cart.forEach(grocery =>{
             let listItem = document.createElement("li");
-            listItem.textContent = `${grocery.quantity} ${grocery.item} ${grocery.dept}`;
+            listItem.textContent = `${grocery.item},       ${grocery.quantity}         ${grocery.unit},       ${grocery.dept}`;
             groceryList.appendChild(listItem);
         });
     }
